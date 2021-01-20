9437608e23e66a2c2a51b8e115d323ca3c878c1d
arya <aryasyama402@gmail.com>
2021-01-20 17:41:04 +0530

    sales done

--- server/server.js
+++ server/server.js
@@ -46,15 +46,70 @@ var product_master = new Schema(
   { versionKey: false }
 );
 
+var sub_master = new Schema(
+  {
+    itemcode: { type: String },
+    item: { type: String },
+    batchno: { type: String },
+    manufacture: { type: String },
+   qty: { type: String },
+   mrp: { type: String },
+   pur_rate: { type: String },
+   exp_date: { type: String },
+    hsn: { type: String },
+    invoiceno: { type: String },
+    invoicedate: { type: String },
+    supplier: { type: String },
+    payment: { type: String },
+    taxamount: { type: String },
+    disper: { type: String },
+    disamnt: { type: String },
+    sgst: { type: String },
+    cgst: { type: String },
+    cess: { type: String },
+    total_gst: { type: String },
+    taxableamnt: { type: String },
+    netamnt: { type: String },
+
+  },
+  { versionKey: false }
+);
+var sale_master = new Schema(
+  {
+    customer: { type: String },
+    name: { type: String },
+    address: { type: String },
+    referred: { type: String },
+    rname: { type: String },
+    addr: { type: String },
+    note: { type: String },
+   date: { type: String },
+   time: { type: String },
+   item: { type: String },
+   invoiceno: { type: String },
+   invoicedate: { type: String },
+   quantity: { type: String },
+   mrp: { type: String },
+   taxpercent: { type: String },
+   discountper: { type: String },
+   discountamnt: { type: String },
+   taxamnt: { type: String },
+   taxableamnt: { type: String },
+   netamnt: { type: String }
+
+   
+  },
+  { versionKey: false }
+);
+
+
+
 var Sample = mongoose.model("store", pro_details, "store");
 
 var ProductMaster = mongoose.model("product_master", product_master, "product_master");
-// var kitty = new ProductMaster({ product: 'Zildjian',manufacture:'macare',hsn:'r456',sch_name:'vfrgt',uom:'vghh',strip:'kkkk',pack:'ttt',loc:'ddd',rack:'trr',subtrack:'ddd',mrp:'200',major_content:'hhhh'});
-// kitty.save(function (err) {
-//   if (err) // ...
-//   console.log('meow');
-// });
-
+var sub_master = mongoose.model("sub_master", sub_master, "sub_master");
+var sale_master = mongoose.model("sale_master", sale_master, "sale_master");
+ 
 //api for get data from database
 app.get("/api/getdata", cors(), function(req, res) {
   Sample.find({}, function(err, data) {
@@ -99,6 +154,37 @@ app.post("/api/form_data", cors(), function(req, res) {
   });
  console.log(req.body);
 });
+
+app.post("/api/mainform_data", cors(), function(req, res) {
+  var newProduct = new sub_master(req.body)
+  newProduct.save(function(err, data) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send({ data: "Record has been Inserted..!!" });
+    }
+  });
+ console.log(req.body);
+});
+
+app.post("/api/sale_form_data", cors(), function(req, res) {
+  var newProduct = new sale_master(req.body)
+  newProduct.save(function(err, data) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send({ data: "Record has been Inserted..!!" });
+    }
+  });
+ console.log(req.body);
+});
+
+
+
+
+
+
+
 
 app.post("/api/savedata", function(req, res) {
   var mod = new model(req.body);
